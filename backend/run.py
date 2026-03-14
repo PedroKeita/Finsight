bec6116d829d7a393383f9e3b18cedf849803bc3
PedroKeita <pedrolucas300805@gmail.com>
2026-03-14 17:16:46 -0300

    feature: Added config for deploy

--- backend/run.py
+++ backend/run.py
@@ -5,5 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 if __name__ == "__main__":
-    port = int(os.getenv("API_PORT", 8000))
-    uvicorn.run("main:app", host="127.0.0.1", port=port, reload=True)
+    port = int(os.getenv("PORT", os.getenv("API_PORT", 8000)))
+    host = os.getenv("HOST", "0.0.0.0")
+    debug = os.getenv("DEBUG", "false").lower() == "true"
+
+    uvicorn.run("main:app", host=host, port=port, reload=debug)
