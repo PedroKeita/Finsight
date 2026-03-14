c7db9d715a1e97226a5e77d664be0d112db2b37e
PedroKeita <pedrolucas300805@gmail.com>
2026-03-14 06:48:25 -0300

    feature: Implemented Return, Volatility, Drawdown and Sharpe in indicators.py

--- backend/indicators.py
+++ backend/indicators.py
@@ -19,4 +19,97 @@ def get_prices(ticker: str, period: str = "1y") -> pd.DataFrame:
 
     interval = period_map.get(period, "365 days")
 
-    query
+    query = text(f"""
+    SELECT p.date, p.close_price
+    FROM prices p
+    JOIN assets a ON a.id = p.asset_id
+    WHERE a.ticker = :ticker
+    AND p.date >= CURRENT_DATE - INTERVAL '{interval}'
+    ORDER BY p.date
+    """)
+    
+    df = pd.read_sql(query, engine, params={"ticker": ticker})
+
+    if df.empty:
+        raise ValueError(f"Nenhum preço encontrado para {ticker}")
+    
+    df["date"] = pd.to_datetime(df["date"])
+    df = df.sort_values("date")
+
+    return df
+
+def calculate_returns(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Calcula os retornos percentuais diários.
+    """
+
+    df ["returns"] = df["close_price"].pct_change()
+    df = df.dropna()
+
+    return df
+
+def cumulative_return(df: pd.DataFrame) -> float:
+    """
+    Retorno acumulado do período.
+    """
+
+    first_price = df["close_price"].iloc[0]
+    last_price = df["close_price"].iloc[-1]
+
+    return (last_price / first_price) - 1
+
+def volatility(df: pd.DataFrame) -> float:
+    """
+    Volatilidade anualizada usando 252 dias úteis.
+    """
+
+    return df["returns"].std() * np.sqrt(TRADING_DAYS)
+
+def max_drawdown(df: pd.DataFrame) -> float:
+    """
+    Calcula o maior drawdown do ativo.
+    """
+
+    cumulative = (1 + df["returns"]).cumprod()
+
+    peak = cumulative.cummax()
+
+    drawdown = (cumulative - peak) / peak
+
+    return drawdown.min()
+
+def sharpe_ratio(df: pd.DataFrame) -> float:
+    """
+    Calcula o Sharpe Ratio usando taxa livre de risco.
+    """
+
+    risk_free_rate = float(os.getenv("RISK_FREE_RATE", 0.13))
+
+    excess_return = (df["returns"].mean() * TRADING_DAYS) - risk_free_rate
+
+    vol = df["returns"].std() * np.sqrt(TRADING_DAYS)
+
+    if vol == 0:
+        return 0
+    
+    return excess_return / vol
+
+def get_indicators(ticker: str, period: str = "1y"):
+    """
+    Retorna todos os indicadores de um ativo.
+    """
+
+    df = get_prices(ticker, period)
+
+    df = calculate_returns(df)
+
+    result = {
+        "return": round(cumulative_return(df) * 100, 2),
+        "volatility": round(volatility(df) * 100, 2),
+        "drawdown": round(max_drawdown(df) * 100, 2),
+        "sharpe": round(sharpe_ratio(df), 2)
+    }
+
+    return result
+
+
